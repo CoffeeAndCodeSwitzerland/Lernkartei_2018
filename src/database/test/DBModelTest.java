3baf00d8045b75ebb7c97d5f6d394929786107d5
MaPf06 <MaPf06@169.254.6.245>
2018-05-25 08:38:25 +0100

    Added todolist for the Databasetest

--- src/database/test/DBModelTest.java
+++ src/database/test/DBModelTest.java
@@ -5,7 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class DBModelTest {
-
+	
+	/* TODO
+	 * Tests to create:
+	 * 1. Create Database
+	 * 2. Create Tables
+	 * 3. Delete Tables
+	 * 4. Add Attributes
+	 * 5. Delete Attributes
+	 * 6. Add data
+	 * 7. Delete data
+	 * 8. Update data
+	 */
+	
+	
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
