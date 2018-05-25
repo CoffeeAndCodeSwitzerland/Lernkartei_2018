d766a5efe986cbbb3874c7e23229adf9321bc82a
WISS <hlucca@gmx.net>
2018-05-25 10:57:02 +0200

    GWT repaired

---
+++ src/persistence/DBGlobalsTest.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DBGlobalsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
